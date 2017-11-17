35ec23fe5b56a25bb6a14d8b6ed145c70bab6d0a
Francisco de Assis Souza Rodrigues <francisco-charlie@hotmail.com>
2017-11-17 15:38:43 -0300

    Update main_0.0.3.py

--- main_0.0.3.py
+++ main_0.0.3.py
@@ -1,5 +1,5 @@
-#coding: utf-8
-#!/usr/bin/python
+# coding: utf-8
+# !/usr/bin/python
 # Python 2/3 compatibility
 
 '''
@@ -16,7 +16,13 @@
     DOC, OpenCV. Smoothing Images. Disponível em: <https://docs.opencv.org/3.1.0/d4/d13/tutorial_py_filtering.html>. Acesso 21/10/2017.
     HIPR2. Zero Crossing Detector. Disponível em: <https://homepages.inf.ed.ac.uk/rbf/HIPR2/zeros.htm>. Acesso 30/10/2017.
     HONG. IMAGE EDGE DETECTION : SOBEL AND LAPLACIAN. Disponível em: <http://www.bogotobogo.com/python/OpenCV_Python/python_opencv3_Image_Gradient_Sobel_Laplacian_Derivatives_Edge_Detection.php>. Acesso 2181082017.
-
+    STACKOVERFLOWS. Improving my Ellipse Fitting Algorithm. Disponível em: <https://stackoverflow.com/questions/21175078/improving-my-ellipse-fitting-algorithm>. Acesso 14/11/2017.
+    PYTHONGEEK. Canny Edge Detection. Disponivel em: <http://pythongeek.blogspot.com.br/2012/06/canny-edge-detection.html>. Acesso 07/11/2017.
+    EDUCAÇÃO, Mundo. Elipse. Disponível em: <http://mundoeducacao.bol.uol.com.br/matematica/elipse.htm>. Acesso 17/11/2017.
+    
+@ DataBase:
+    ARCHIVE, Computational Vision. Faces 1999 (Front). Disponível em: <http://www.vision.caltech.edu/html-files/archive.html>. Acesso 15/11/2017.
+    SPACEK, Libor. Description of the Collection of Facial Images. Disponível em: <http://cswww.essex.ac.uk/mv/allfaces/index.html>. Acesso 13/11/2017.
 '''
 
 
@@ -34,23 +40,37 @@ import time
 from random import choice
 
 
+'''
+Tempo de execução
+'''
+START_TIME = time.time()
 
-start_time = time.time()
 
-
-
-sigma = 2
+'''
+variáveis globais
+'''
+SIGMA = 2,2
 VIEWIMG = 0
 
 
-# read img_orig
-filenames = [i for i in glob.glob("../src/img/input/*.jpg")]
-filenames.sort() # ADD THIS LINE
+'''
+    Configuração do caminho do diretório de imagens.
+'''
+dir_files = '../src/img/input/database/test/simples'
+#dir_files = '../src/img/input/database/test/complexo'
 
+#dir_files = '../src/img/input/database/bd_01/complexo'
+#dir_files = '../src/img/input/database/bd_02/complexo'
+#dir_files = '../src/img/input/database/bd_02/simples'
+
+img_file = [i for i in glob.glob(dir_files+"/*.jpg")]
+img_file.sort()
 images = []
 
+
+
 '''
-Gerador de ID para salvar as imagens processadas.
+    Gerador de código de identificação, para salvar as imagens processadas.
 '''
 def randomIDImg(size):
     data = '0123456789'
@@ -62,8 +82,11 @@ def randomIDImg(size):
     return  code
 
 
+
 '''
-Exibe imagens processadas na tela.
+    Exibe imagens processadas na tela.
+    Usar, somente em modo de teste, quando estiver exibindo as imagens na janela.
+    Recomenda-se testar com no MAX. 5 imagens, risco de deadlock.
 '''
 def showImg(title, img):
     if(VIEWIMG):
@@ -72,21 +95,31 @@ def showImg(title, img):
 
 
 '''
-Loop
+    Loop
 '''
-print('processing..')
+print('Start processing..')
 
-for i in filenames:
+for i in img_file:
 
     id_img = randomIDImg(10)
 
     n = cv2.imread(i)
     images.append(n)
-    print('file: '+i)
+    print('file processing: '+i)
 
 
     img_orig = cv2.imread(i, 0)
     showImg('Image Original', img_orig)
+
+    # Verifica se a imgagem é RGB antes de converter para escala de cinza.
+    if img_orig.shape[-1] == 3:                    # color image
+        b, g, r = cv2.split(img_orig)              # get b, g, r
+        img_orig = cv2.merge([r, g, b])            # switch it to rgb
+        img_gray = cv2.cvtColor(img_orig, cv2.COLOR_BGR2GRAY)
+
+        showImg('Image Cinza', img_gray)
+    else:
+        img_gray = img_orig
 
 
 
@@ -97,8 +130,8 @@ for i in filenames:
     de iluminação. Nesse passo, a equalização do histograma é usada para melhorar o contraste 
     da imagem original.
     '''
-    img_hist = cv2.equalizeHist(img_orig)
-    cv2.imwrite('../src/img/output/'+id_img+'_img_hist.jpg', img_hist)
+    img_hist = cv2.equalizeHist(img_gray)
+    cv2.imwrite('../src/img/output/processing/'+id_img+'_img_hist.jpg', img_hist)
     showImg('Step 01 - Image Enhancement (Equalize Histogram)', img_hist)
 
 
@@ -111,10 +144,10 @@ for i in filenames:
     Por meio da filtragem, podem ser apagados as fontes de ruídos presente na imagem, aplicando um
     median filtering.
     '''
-    img_hist = cv2.imread('../src/img/output/'+id_img+'_img_hist.jpg', 0)
+    img_hist = cv2.imread('../src/img/output/processing/'+id_img+'_img_hist.jpg', 0)
     img_blur = cv2.medianBlur(img_hist, 5)
 
-    cv2.imwrite('../src/img/output/'+id_img+'_img_blur.jpg', img_blur)
+    cv2.imwrite('../src/img/output/processing/'+id_img+'_img_blur.jpg', img_blur)
     showImg('Step 02 - Median Filtering', img_blur)
 
 
@@ -126,11 +159,11 @@ for i in filenames:
     o zero-crossing detector: Canny Edge Detection (DoG).
     '''
 
-    img_blur = '../src/img/output/'+id_img+'_img_blur.jpg'
+    img_blur = '../src/img/output/processing/'+id_img+'_img_blur.jpg'
     img_blur = Image.open(img_blur)
     img_data = np.array(img_blur, dtype = float)
 
-    img_median = ndi.filters.median_filter(img_data, sigma)
+    img_median = ndi.filters.median_filter(img_data, SIGMA)
 
     # imagem vazia
     sobelout = Image.new('L', img_blur.size)
@@ -173,8 +206,8 @@ for i in filenames:
     sobeloutmag = scipy.hypot(gradx, grady)
     sobeloutdir = scipy.arctan2(grady, gradx)
 
-    scipy.misc.imsave('../src/img/output/'+id_img+'_img_cannynewmag.jpg', sobeloutmag)
-    scipy.misc.imsave('../src/img/output/'+id_img+'_img_cannynewdir.jpg', sobeloutdir)
+    scipy.misc.imsave('../src/img/output/processing/'+id_img+'_img_cannynewmag.jpg', sobeloutmag)
+    scipy.misc.imsave('../src/img/output/processing/'+id_img+'_img_cannynewdir.jpg', sobeloutdir)
 
     for x in range(width):
         for y in range(height):
@@ -191,14 +224,15 @@ for i in filenames:
             else:
                 sobeloutdir[x][y]=135
 
-    scipy.misc.imsave('../src/img/output/'+id_img+'_img_cannynewdirquantize.jpg', sobeloutdir)
+    scipy.misc.imsave('../src/img/output/processing/'+id_img+'_img_cannynewdirquantize.jpg', sobeloutdir)
 
 
 
     '''
     Step 04 - Edge Linking
 
-    Conecta as bordas pequenas da imagem com as bordas grandes, com base na direção das bordas.
+    Conecta as bordas pequenas da imagem com as bordas grandes, com base na direção das bordas, por meio do gradiene de bordas com 
+    o operador Sobel.
     '''
     mag_sup = sobeloutmag.copy()
 
@@ -221,7 +255,7 @@ for i in filenames:
                    (sobeloutmag[x][y]<=sobeloutmag[x-1][y-1]):
                     mag_sup[x][y]=0
 
-    scipy.misc.imsave('../src/img/output/'+id_img+'_img_cannynewmagsup.jpg', mag_sup)
+    scipy.misc.imsave('../src/img/output/processing/'+id_img+'_img_cannynewmagsup.jpg', mag_sup)
 
     m = np.max(mag_sup)
     th = 0.2*m
@@ -237,12 +271,12 @@ for i in filenames:
             if mag_sup[x][y]>=tl:
                 gnl[x][y]=mag_sup[x][y]
 
-    scipy.misc.imsave('../src/img/output/'+id_img+'.jpg', gnl)
+    scipy.misc.imsave('../src/img/output/processing/'+id_img+'.jpg', gnl)
 
     gnl = gnl-gnh
 
-    scipy.misc.imsave('../src/img/output/'+id_img+'_img_cannynewgnlafterminus.jpg', gnl)
-    scipy.misc.imsave('../src/img/output/'+id_img+'_img_cannynewgnh.jpg', gnh)
+    scipy.misc.imsave('../src/img/output/processing/'+id_img+'_img_cannynewgnlafterminus.jpg', gnl)
+    scipy.misc.imsave('../src/img/output/processing/'+id_img+'_img_cannynewgnh.jpg', gnh)
 
 
 
@@ -260,7 +294,7 @@ for i in filenames:
                 gnh[i][j]=1
                 traverse(i, j)
 
-    scipy.misc.imsave('../src/img/output/'+id_img+'_img_cannynewout.jpg', gnh)
+    scipy.misc.imsave('../src/img/output/processing/'+id_img+'_img_cannynewout.jpg', gnh)
 
     showImg('Step 03/04 - Edge Detection/Edge Linking', gnh)
 
@@ -270,10 +304,13 @@ for i in filenames:
     Step 05 - Template Matching
 
     '''
-    edge_image = cv2.imread('../src/img/output/'+id_img+'_img_cannynewout.jpg', 0)
+    cont_face = 0
+    edge_image = cv2.imread('../src/img/output/processing/'+id_img+'_img_cannynewout.jpg', 0)
     
     img_dilate = cv2.dilate(edge_image, np.ones((3, 3)), iterations=1)
-    cv2.imshow('Dilate', img_dilate)
+    #img_eroded = cv2.erode(img_dilate, np.ones((3, 3)), iterations=7)
+
+    showImg("Dilate", img_dilate)
 
     labels, count = ndi.label(img_dilate)
 
@@ -282,19 +319,57 @@ for i in filenames:
         sx = idx[1].start
         y, x = np.where(labels[idx] == lab)
         ellp = cv2.fitEllipse(np.column_stack((x+sx, y+sy)))
-        cv2.ellipse(img_orig, ellp, (0, 255, 0))
+
+        '''
+            @ Parâmetros de uma elipse
+                
+                xc: coordenada x do centro
+                yc: coordenada y do centro
+                a: semi-eixo principal
+                b: semi-eixo secundário
+                theta: ângulo de rotação
+        '''
+        (xc, yc), (a, b), theta = cv2.fitEllipse(np.column_stack((x+sx, y+sy)))
+        
+        # Verificar se é uma elipse.
+        isElipse = ( (xc**2) / (b**2) ) + ( (yc**2) / (a**2) )
+
+        # Mostra caracgteristicas da elipse.
+        print("value: ", isElipse)
+        print("xc: ", xc)
+        print("yc: ", yc) 
+        print("a: ", a)
+        print("b: ", b)
+        print("theta: ", theta)
+        print("ellp", ellp)
+
+        if(isElipse <= 1):
+            img_orig = cv2.ellipse(img_orig, ellp, (255, 255, 255), 1)
+            img_dilate = cv2.ellipse(img_dilate, ellp, (255, 255, 255), 1)
+            cont_face+=1
+            print('É uma elipse :) ')
+            print "I found {0} face(s) in that image".format(cont_face)
+        else: 
+            print('Não é uma elipse :( ')
 
     scipy.misc.imsave('../src/img/output/result/'+id_img+'_img_detection.jpg', img_orig)
-    cv2.imshow('Step 05 - Template Matching', img_orig)
+    scipy.misc.imsave('../src/img/output/result/'+id_img+'_img_detection2.jpg', img_dilate)
+
+    showImg("Step 05 - Template Matching", img_orig)
 
 
 
-end_time = time.time()
-time_taken = end_time - start_time
-print "Time taken for execution: ", time_taken
+END_TIME = time.time()
+TIME_TAKEN = END_TIME - START_TIME
+
+print('finish')
+print 'Time taken for execution: ', TIME_TAKEN
 
 
 
-# Exit
+'''
+Exit
+Usar, somente em modo de teste, quando estiver exibindo as imagens na janela.
+'''
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
